97202ff07d934ccc136b90749e96368fd14d96cf
Manivannan Prushorth <manivanann.prushorth@kcl.ac.uk>
2022-04-13 09:48:23 +0100

    Add mprotect.c and munprotect.c

---
+++ mprotect.c
@@ -0,0 +1,16 @@
+#include "param.h"
+#include "types.h"
+//#include "defs.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "elf.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+  // int x = 8;
+  // mprotect(&x, 2);
+  // exit();
+  return 0;
+}
---
+++ munprotect.c
@@ -0,0 +1,15 @@
+#include "param.h"
+#include "types.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "elf.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+  // int x = 8;
+  // munprotect(&x, 2);
+  // exit();
+  return 0;
+}
